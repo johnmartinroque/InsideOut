476cbfa7523a83737e67538d2a7cc8a77ac00410
johnmartinroque <johnmartinroque@gmail.com>
2025-08-05 22:30:30 +0800

    bootstrap

--- main/src/screens/Home.jsx
+++ main/src/screens/Home.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Container className="vh-100">
+        <Row className="h-100 d-flex  ">
+          <Col className="text-center">
+            <h1>Inside Out</h1>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default Home;

6fcef2412ea0a36016ec7d695c0feb71158e0ff4
johnmartinroque <johnmartinroque@gmail.com>
2025-09-16 14:51:59 +0800

    add companion form

--- main/src/components/AddCompanion.jsx
+++ main/src/components/AddCompanion.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 function AddCompanion() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const addCompanion = async () => {};
   return (
     <Form
       style={{
@@ -15,19 +20,13 @@ function AddCompanion() {
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>Email address</Form.Label>
         <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
         <Form.Control type="password" placeholder="Password" />
       </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onClick={addCompanion}>
         Submit
       </Button>
     </Form>

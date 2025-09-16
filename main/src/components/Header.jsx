9062a980f2bd49286773dfa07a4649a07c5b6636
johnmartinroque <johnmartinroque@gmail.com>
2025-09-16 15:06:48 +0800

    user logout

--- main/src/components/Header.jsx
+++ main/src/components/Header.jsx
@@ -1,9 +1,21 @@
+import { getAuth, signOut } from "firebase/auth";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
+
 function Header() {
+  const auth = getAuth();
+  const handleSignout = async () => {
+    await signOut(auth);
+    localStorage.removeItem("user");
+    console.log("User signed out successfully.");
+    try {
+    } catch (err) {
+      console.error("Error signing out: ", err);
+    }
+  };
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
@@ -30,8 +42,8 @@ function Header() {
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item onClick={handleSignout}>
+                Logout
               </NavDropdown.Item>
             </NavDropdown>
           </Nav>

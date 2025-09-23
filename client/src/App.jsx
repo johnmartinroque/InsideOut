6efff89e93daec0a2733f106f64e4d29de34cce3
johnmartinroque <johnmartinroque@gmail.com>
2025-09-23 18:44:09 +0800

    login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
+import Header from "./components/Header";
 function App() {
   return (
     <div>
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div>
       <div className="flex flex-col items-center">
-        <h1>HOME</h1>
+        <h1 className="text-5xl">HOME</h1>
       </div>
     </div>
   );

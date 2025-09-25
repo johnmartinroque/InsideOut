9cc862d9e2f6443ba0647a7507fc9d414ee1ad52
johnmartinroque <johnmartinroque@gmail.com>
2025-09-25 21:33:30 +0800

    user email in header after logging in

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,10 @@
-import { getAuth, signOut } from "firebase/auth";
-import React from "react";
+import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { auth } from "../firebase";
 
 function Header() {
+  const [user, setUser] = useState(null);
   const handleLogout = async () => {
     try {
       await signOut(auth);
@@ -12,6 +13,16 @@ function Header() {
       console.error(err);
     }
   };
+
+  useEffect(() => {
+    // listen for changes in auth state
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <div>
       <header>
@@ -90,6 +101,14 @@ function Header() {
                     class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
                   >
                     Contact
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    class="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
+                  >
+                    {user ? user.email : "Guest"}
                   </a>
                 </li>
                 <li>

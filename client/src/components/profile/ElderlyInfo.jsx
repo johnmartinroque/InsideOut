f77973a57644980257395845f347621979deda52
johnmartinroque <johnmartinroque@gmail.com>
2025-09-28 13:55:42 +0800

    fetch elderly info setup

--- client/src/components/profile/ElderlyInfo.jsx
+++ client/src/components/profile/ElderlyInfo.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import db, { collection, getDoc, getDocs } from "firebase/firestore";
 
 function ElderlyInfo() {
+  const fetchElderly = async () => {
+    const elderlyCollectionRef = collection(db, "elderly");
+    const elderly = await getDoc(elderlyCollectionRef);
+  };
+
   return (
-    <div className="flex flex-col items-center">
-      <div className="bg-white shadow-sm border border-slate-200 rounded-lg w-96 h-100 p-6"></div>
+    <div>
+      <div className="flex flex-col  items-center bg-white shadow-sm border border-slate-200 rounded-lg w-96 h-100 p-6 gap-10">
+        <h1>Elderly Name</h1>
+        <p>Account Email</p>
+        <p>091223141412</p>
+        <p>Companion 1</p>
+        <p>Companion 2</p>
+        <p>Companion 3</p>
+      </div>
     </div>
   );
 }
